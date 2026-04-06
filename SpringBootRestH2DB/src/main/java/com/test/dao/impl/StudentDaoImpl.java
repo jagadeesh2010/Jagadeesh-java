35a1b8c94c3cd73680637cc11ccb9b5adb8455e6
jagadeesh2010 <jagadeeshgalgin@gmail.com>
2026-04-06 17:00:12 +0530

    Add files via upload

---
+++ SpringBootRestH2DB/src/main/java/com/test/dao/impl/StudentDaoImpl.java
@@ -0,0 +1,55 @@
+package com.test.dao.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.test.dao.StudentDao;
+import com.test.entity.Student;
+import com.test.repository.StudentRespository;
+
+@Repository
+public class StudentDaoImpl implements StudentDao
+{
+	@Autowired
+	private StudentRespository repository;
+
+	@Override
+	public Student saveStudent(Student obj) {
+	
+		return repository.save(obj);
+	}
+
+	@Override
+	public Student getStudentById(int id) {
+		
+		return repository.findById(id).get();
+	}
+
+	@Override
+	public Student updateStudentById(Student obj) {
+		// TODO Auto-generated method stub
+		return repository.save(obj);
+	}
+
+	@Override
+	public List<Student> deleteStudentById(int id) {
+		
+		repository.deleteById(id);
+		
+		List<Student> list = repository.findAll();
+		return list;
+	}
+
+	@Override
+	public List<Student> getAllStudents() {
+		
+		return repository.findAll();
+	}
+
+	
+	
+	
+	
+}
---
+++ SpringBootRestH2DB/src/main/java/com/test/repository/StudentRespository.java
@@ -0,0 +1,17 @@
+package com.test.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.test.entity.Student;
+
+
+
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+public interface StudentRespository extends JpaRepository<Student, Integer>
+{
+	
+
+}
